74f32c4b33aa1e16ba0de1fb7da172e046412e1f
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2021-11-29 12:21:10 +0800

    Update

--- server/app/Notifications/AppointmentVaccineCreated.php
+++ server/app/Notifications/AppointmentVaccineCreated.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class AppointmentVaccineCreated extends Notification
+class AppointmentVaccineCreated extends Notification implements ShouldQueue
 {
     use Queueable;
 
--- server/app/Notifications/VerifySMS.php
+++ server/app/Notifications/VerifySMS.php
@@ -2,14 +2,18 @@
 
 namespace App\Notifications;
 
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Str;
 
-class VerifySMS extends Notification
+class VerifySMS extends Notification implements ShouldQueue
 {
+    use Queueable;
+
     /**
      * Get the notification's delivery channels.
      *
